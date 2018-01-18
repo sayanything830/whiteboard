17cf388b6d32c9dbcbb2ec2e8f6ba473b788217a
Melanie Downing <sayanything830@gmail.com>
2018-01-18 14:41:17 -0800

    finished readme, fixed file path in index, added photo

--- challenge-04/index.js
+++ challenge-04/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
-const intersect = require('../lib/intersect.js');
+const intersect = require('./lib/intersect.js');
 
 intersect.intersector([1, 2, 3, 4, 5], [2, 3, 6, 7, 8, 9]);
+console.log(intersect.intersector([1, 2, 3, 4, 5], [2, 3, 6, 7, 8, 9])
+);
