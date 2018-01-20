490c78f008fcbb22920036b22a7b5551fd3b2d1e
Melanie Downing <sayanything830@gmail.com>
2018-01-19 19:58:10 -0800

    tests pass

--- challenge-05/__test__/solution.test.js
+++ challenge-05/__test__/solution.test.js
@@ -4,3 +4,47 @@
 //should return null if not two keys in object
 //should return null if typeof argument is not object
 //should return valid object in middle of linked list
+
+const solution = require('../lib/solution.js');
+
+describe('Solution Module', function() {
+  describe('#Find Middle Node', () => {
+
+    //valid single linked list
+    let sllOne = {
+      value: 'a',
+      next: {
+        value:'b',
+        next: {
+          value: 'c',
+          next: null,
+        },
+      },
+    };
+
+    //missing 'next' key in last object
+    let sllTwo = {
+      value: 1,
+      next: {
+        value:2,
+        next: {
+          value: 3,
+        },
+      },
+    };
+
+    it('should return the valid middle node', () => {
+      expect(solution.findMiddleNode(sllOne)).toEqual({ value: 'b', next: { value: 'c', next: null } });
+      console.log(solution.findMiddleNode(sllOne));
+    });
+    it('should return null if two keys do not exist per node', () => {
+      expect(solution.findMiddleNode(sllTwo)).toBeNull();
+    });
+    it('should return null if argument passed is not an object', () => {
+      expect(solution.findMiddleNode(1)).toBeNull();
+    });
+    it('should return null if no argument is passed', () => {
+      expect(solution.findMiddleNode()).toBeNull();
+    });
+  });
+});
--- challenge-05/lib/solution.js
+++ challenge-05/lib/solution.js
@@ -4,7 +4,17 @@ const solution = module.exports = {};
 
 
 solution.findMiddleNode = function(sll) {
+  if(!sll) return null;
+
   var itrMid, itrEnd;
+
+  for(itrEnd = sll; itrEnd; itrEnd = itrEnd.next) {
+    if(typeof itrEnd !== 'object') return null;
+
+    let key = Object.keys(itrEnd);
+    if(key.length !== 2) return null;
+  }
+
 
   for(itrMid = sll, itrEnd = sll; itrEnd.next && itrEnd.next.next; itrMid = itrMid.next, itrEnd = itrEnd.next.next);
 
